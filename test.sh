30df37d5f527925c120428740d00aa3106a559c6
Roberto Alves <roberto@rhfactor.com.br>
2022-07-13 17:51:47 -0300

    Adicionar uma nova thread com outro email para ver se ela é liberada antes das demais

--- test.sh
+++ test.sh
@@ -2,3 +2,4 @@ curl "http://localhost:8080/verify?email=email1&source=1" & \
   curl "http://localhost:8080/verify?email=email1&source=2" & \
   curl "http://localhost:8080/verify?email=email1&source=3" & \
   curl "http://localhost:8080/verify?email=email1&source=4" & \
+  curl "http://localhost:8080/verify?email=email2&source=5" & \
